e32fa99b8ace02cdffad5683a0c363e13dd71399
dorKnight <kartheeskofficial@gmail.com>
2024-11-09 03:25:03 +0530

    comment

--- bigram.py
+++ bigram.py
@@ -11,7 +11,7 @@ learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 
-torch.manual_seed(1337) #manual seed
+torch.manual_seed(1337) #comment
 
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
